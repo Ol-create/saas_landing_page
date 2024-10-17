43cc34b623118daf9fc4e70e1f59baeea59ea2f5
Ol-create <oluola96@gmail.com>
2024-10-17 06:14:24 +0100

    create hero section

--- saas_project/src/App.jsx
+++ saas_project/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 
 
 import Header from './sections/Header'
+import Hero from './sections/Hero'
 
 function App() {
 
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   )
 }
